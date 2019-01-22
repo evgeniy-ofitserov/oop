b24cc86fd6652c1cd3befd745488dcc205f2c7d2
evgeniy-ofitserov <iva345644@gmail.com>
2019-01-22 14:34:39 +0300

    Выбор данных из БД с защитой

--- index.php
+++ index.php
@@ -1,103 +1,142 @@
 <?php 
 // Подключение через PDO
-
 // тип БД и подкл. 'mysql:host=localhost; dbname=wd04-filmoteka-ofitserov
 
-$db = new PDO('mysql:host=localhost;dbname=wd04-filmoteka-ofitserov','root', '');
+// Создаем объект
+
+ $db = new PDO('mysql:host=localhost;dbname=mini-site','root', '');
+
+
+// // 1. Выборка без защиты от SQL инъекций
+
+// $name = 'Joker';
+// $password = '777';
+
+// // Делаем запрос в БД и записываем все в переменную sql
+
+// $sql = "SELECT * FROM users WHERE name ='{$name}' AND password = '{$password}' LIMIT 1";
+// $result = $db->query($sql);
+
+// echo "<h2>Выборка записи без защиты от SQL инъекций:</h2>";
+// // print_r( $result->fetch(PDO::FETCH_ASSOC) );
 
 
 
-// Создаем запрос к БД
+// // проверяем методом rowCount (возвращает количество строк, которые были затронуты в ходе запроса)
+// if ($result->rowCount() == 1) {
 
-// $sql = "SELECT * FROM films";
-
-
-// Выполняем запрос спец.методом query
-
-// $resault = $db->query($sql);
-
-// echo "<h2> Вывод записей из результата по одной: </h2> ";
-
-
-// // Вывод методом fetch по одному рез. каждой строки
-
-// // $resault->fetch(PDO::FETCH_ASSOC);
-
-// // print_r($resault->fetch(PDO::FETCH_ASSOC));
-
-
-
-// // Выводим все фильмы и записываем в переменную $film
-
-// while ($film = $resault->fetch(PDO::FETCH_ASSOC)) {
-// 	// print_r($film);
-
-// 	echo "<br><br><br><br><br> ";
-
-
-// 	echo "Название фильма: " . $film['title'] . "<br>";
-// 	echo "Жанр фильма: " . $film['genre'] . "<br>";
-// 	echo "Год фильма: " . $film['years'] . "<br>";
-// 	echo "Имя пути картинки  " . $film['photo'] . "<br>";
-
-// 	echo "<br><br><br><br><br> ";
-
-
+// 	$user =  $result->fetch(PDO::FETCH_ASSOC);
+// 	echo "Имя пользователя {$user['name']} <br>";
+// 	echo "Пароль пользователя {$user['password']}";
 // }
 
-echo "<hr/>";
-
-// $sql = "SELECT * FROM films";
-// $resault = $db->query($sql);
-
-// // Второй способ (Подключение то же), получаем все данные сразу
-
-// $films = $resault->fetchAll(PDO::FETCH_ASSOC);
-
-// // print_r($resault->fetchAll(PDO::FETCH_ASSOC));
 
 
-// // Обходим массивом foreach  полученные данные которые в переменной $films 
 
-// foreach ( $films as $film) {
-// 	echo "Название фильма: " . $film['title'] . "<br>";
-// 	echo "Жанр фильма: " . $film['genre'] . "<br>";
-// 	echo "Год фильма: " . $film['years'] . "<br>";
-// 	echo "Имя пути картинки  " . $film['photo'] . "<br>";
-// 	echo "<hr/>";
-// 	echo "<br/>";
+// echo "<br><br><br><br><br>";
+// echo "<hr/>";
 
+
+// // 2. Выборка без защиты от SQL инъекций РУЧНОЙ РЕЖИМ 
+
+// echo "<h2>Выборка записи c защитой от SQL инъекций - (РУЧНОЙ РЕЖИМ!!!):</h2>";
+
+// $username = 'Joker';
+// $password = '777';
+
+// // Метод quote защищает от пробелов
+
+// $username = $db->quote($username);
+// // strtr - Экранирует символы, записывает их в строчку
+// $username = strtr($username, array('_' => '\_','%' => '\%') );
+
+
+// $password = $db->quote($password);
+// $password = strtr($password, array('_' => '\_','%' => '\%') );
+
+
+// $sql = "SELECT * FROM users WHERE name ='{$name}' AND password = '{$password}' LIMIT 1";
+// $result = $db->query($sql);
+
+
+// if ($result->rowCount() == 1) {
+
+// 	$user =  $result->fetch(PDO::FETCH_ASSOC);
+// 	echo "Имя пользователя {$user['name']} <br>";
+// 	echo "Пароль пользователя {$user['password']}";
 // }
 
-echo "<hr/>";
+// echo "<br>";
+
+
+// echo "<h2>Выборка записи c защитой от SQL инъекций - (АВТОМАТИЧЕСКИЙ РЕЖИМ!!!):</h2>";
+
+
+// Создаем запрос, где подготавливаем наши переменные :username, :password
+
+// $sql = "SELECT * FROM users WHERE name = :username AND password = :password LIMIT 1";
+
+// Создаем переменную, которая подготавливает запрос. Есть метод prepare
+
+
+// $stmt = $db->prepare($sql);
+
+// $username = 'Joker';
+// $password = '777';
+
+// подставляем полученные данные в наш sql запрос.Вызываем метод bindValue
+
+// $stmt->bindValue(':username',$username);
+// $stmt->bindValue(':password',$password);
+
+// Получаем готовый запрос методом execute (массив ассоц.);
+// $stmt->execute();
+
+// вариант 2 без bindValue
+
+//  $stmt->execute(array(':username' => $username, ':password' => $password));
+
+// $stmt->bindColumn('name', $name);
+// $stmt->bindColumn('email', $email);
+
+// echo "<h3> Выборка записи с авт.защитой от SQL - инъекций</h3>";
+
+// $stmt->fetch();
+
+// echo "Имя пользователя {$name} <br>";
+// echo "Пароль пользователя {$email}";
 
 
 
-// Третий способ
+// Другой вариант
+
+$sql = "SELECT * FROM users WHERE name = ? AND password = ? LIMIT 1";
+
+// подготавливаем SQL запрос 
+
+$stmt = $db->prepare($sql);
 
 
-$sql = "SELECT * FROM films";
-$resault = $db->query($sql);
+$username = 'Joker';
+$password = '777';
 
-// Обращаемся к объекту и с помощью метода bindColumn. Указываем какие переменные (обращение к БД) нужно создать
+$username = htmlentities($username);
+$password = htmlentities($password);
 
-$resault->bindColumn('id', $id);
-$resault->bindColumn('title', $title);
-$resault->bindColumn('genre', $genre);
-$resault->bindColumn('years', $years);
-$resault->bindColumn('photo', $photo);
+$stmt->bindValue(1, $username);
+$stmt->bindValue(2, $password);
 
-echo "<h2> Вывод записей с привязкой данных к переменным </h2>";
-	while ($resault->fetch(PDO::FETCH_ASSOC)) {
-		echo "ID: {$id}<br>";
-		echo "Название: {$title}<br>";
-		echo "Жанр: {$genre}<br>";
-		echo "Год: {$years}<br>";
-		echo "Имя картинки: {$photo}<br>";
-
-		echo "<br><br>";
-	}
+// Получаем готовый запрос методом execute (массив ассоц.);
+$stmt->execute();
+$stmt->bindColumn('name', $name);
+$stmt->bindColumn('email', $email);
 
 
+echo "<h3> Выборка записи с авт.защитой от SQL - инъекций</h3>";
+
+$stmt->fetch();
+
+echo "Имя пользователя {$name} <br>";
+echo "Пароль пользователя {$email}";
 
 ?>
